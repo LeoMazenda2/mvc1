41f246a02f22df59edeefe68fb71dc1ab935c185
Leonildo Mazenda <leonildo.mazenda@hotmail.com>
2025-01-28 17:14:09 +0100

    Dockerfile.multi

--- obj/Debug/net6.0/mvc1.AssemblyInfo.cs
+++ obj/Debug/net6.0/mvc1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+501f72b6e69e279827c20c842fe9b1d5fca8100d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aa60dad2021114393018c99ca79c442a980f419")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
